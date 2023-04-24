44f6e5afdcc3539f753896b6e631e760fae06966
kalkidan12 <kalget12@gmail.com>
2023-04-24 11:14:42 +0300

    init

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BloC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Text(''),
+    );
+  }
+}
